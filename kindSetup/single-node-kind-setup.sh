d86c84d1fbacbb4cbae10915e6a294435aa0f4d2
Mishra, Manu <Manu.Mishra>
2022-01-05 17:18:34 -0600

    changes

---
+++ kindSetup/single-node-kind-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# credits to https://github.com/neumanndaniel/kubernetes/blob/master/kind/setup.sh
+
+set -e
+
+kind create cluster --config=single-node.yaml --name dev1
+
+# Calico
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl apply -f ./calico-config.yaml
+
+sleep 120
+
+# Metrics Server
+kubectl config set-context --current --namespace kube-system
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade metrics-server --install \
+--set apiService.create=true \
+--set extraArgs.kubelet-insecure-tls=true \
+--set extraArgs.kubelet-preferred-address-types=InternalIP \
+bitnami/metrics-server --namespace kube-system
