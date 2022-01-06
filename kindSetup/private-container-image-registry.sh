f5406ef1c2977b5439a730340ab7d77adad48139
Mishra, Manu <Manu.Mishra>
2022-01-05 19:07:12 -0600

    linux eof

--- kindSetup/private-container-image-registry.sh
+++ kindSetup/private-container-image-registry.sh
@@ -1,2 +1,4 @@
 kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra
 kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra -n development
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra -n stage
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra -n production
