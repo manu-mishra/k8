2c056887669638df29a3ad5b24f4c2d813a4da33
Mishra, Manu <Manu.Mishra>
2022-01-06 09:49:24 -0600

    hiding keys

--- autopilot/recover-from-repo/recover.sh
+++ autopilot/recover-from-repo/recover.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # credits to https://github.com/argoproj-labs/argocd-autopilot/issues/26
-cd ..
-cd ..
-cd gitops
 
  kubectl create namespace argocd
 
-kustomize build bootstrap/argo-cd | kubectl apply -f -
-kubectl apply -f bootstrap/argo-cd.yaml
-kubectl apply -f bootstrap/root.yaml
-kubectl apply -f bootstrap/cluster-resources.yaml
-kubectl -n argocd create secret generic autopilot-secret --from-literal git_username=username --from-literal git-token=ghp_...SglPq
-
+kustomize build ../gitops/bootstrap/argo-cd | kubectl apply -f -
+kubectl apply -f ../gitops/bootstrap/argo-cd.yaml
+kubectl apply -f ../gitops/bootstrap/root.yaml
+kubectl apply -f ../gitops/bootstrap/cluster-resources.yaml
+kubectl -n argocd create secret generic autopilot-secret --from-literal git_username=username --from-literal git-token=ghp_...lPq
+sleep 10
 kubectl get secret argocd-initial-admin-secret -n argocd -ogo-template='{{printf "%s\n" (index (index . "data") "password" | base64decode)}}'
--- kindSetup/private-container-image-registry.sh
+++ kindSetup/private-container-image-registry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_...31V --docker-email=manu-mishra
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_...31V --docker-email=manu-mishra -n development
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_...31V --docker-email=manu-mishra -n stage
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_...31V --docker-email=manu-mishra -n production
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_pW31V --docker-email=manu-mishra
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_pW31V --docker-email=manu-mishra -n development
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_pW31V --docker-email=manu-mishra -n stage
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_pW31V --docker-email=manu-mishra -n production
