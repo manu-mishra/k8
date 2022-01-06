d346e2321c0951558baeee4a05db714fb146daa7
Mishra, Manu <Manu.Mishra>
2022-01-05 18:51:06 -0600

    recovery

---
+++ autopilot/recover-from-repo/readme.md
@@ -0,0 +1,5 @@
+## install kinD
+
+## install Kustomize
+1. curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash  
+2. sudo install -o root -g root -m 0755 kustomize /usr/local/bin/kustomize  
---
+++ kindSetup/private-container-image-registry.sh
@@ -0,0 +1,2 @@
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=ghp_p....31V --docker-email=manu-mishra -n development
