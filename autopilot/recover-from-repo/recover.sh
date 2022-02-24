7aead00765bd21220a6360846fc0227c3758d8d7
Manu Mishra <mishra.manu@outlook.com>
2022-02-24 00:20:37 -0600

    commit

--- autopilot/recover-from-repo/recover.sh
+++ autopilot/recover-from-repo/recover.sh
@@ -6,10 +6,10 @@ kubectl create namespace argocd
 
 kustomize build ../gitops/bootstrap/argo-cd | kubectl apply -f -
 kubectl apply -f ../gitops/bootstrap/argo-cd.yaml
-kubectl wait --for=condition=available --timeout=600s deployment.apps/argocd-server -n argocd
+kubectl wait --for=condition=available --timeout=6s deployment.apps/argocd-server -n argocd
 kubectl -n argocd create secret generic autopilot-secret --from-literal git_username=username --from-literal git-token=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
 kubectl apply -f ../gitops/bootstrap/cluster-resources.yaml
 kubectl apply -f ../gitops/bootstrap/operations-root.yaml
-kubectl get secret argocd-initial-admin-secret -n argocd -ogo-template='{{printf "%s\n" (index (index . "data") "password" | base64decode)}}'
-#kubectl apply -f ../gitops/bootstrap/app-root.yaml
+#kubectl get secret argocd-initial-admin-secret -n argocd -ogo-template='{{printf "%s\n" (index (index . "data") "password" | base64decode)}}'
+kubectl apply -f ../gitops/bootstrap/app-root.yaml
 
