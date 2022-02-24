2224b22a31a70a0ec9b4ace3b9cfc409b114ab46
Manu Mishra <mishra.manu@outlook.com>
2022-02-24 00:48:04 -0600

    t

--- kindSetup/private-container-image-registry.sh
+++ kindSetup/private-container-image-registry.sh
@@ -6,8 +6,8 @@ kubectl delete secret docker-registry private-repo-secret -n stage
 kubectl delete secret docker-registry private-repo-secret -n production
 kubectl delete secret docker-registry private-repo-secret -n operations
 
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n development
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n stage
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n production
-kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n operations
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n development
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n stage
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n production
+kubectl create secret docker-registry private-repo-secret --docker-server=https://ghcr.io --docker-username=manu-mishra --docker-password=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 --docker-email=manu-mishra -n operations
